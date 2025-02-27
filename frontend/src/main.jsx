3d9aeb927067ba7a0c793c159375e23263d547c0
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-02-28 01:10:55 +0530

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
-
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById("root")).render(
   <>
     <ToastContainer
